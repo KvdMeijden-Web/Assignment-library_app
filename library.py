179eaf0abcaeeb0cca3f1f832c322593e2eb564a
KvdMeijden-Web <k.vandermeijden@isalmere.nl>
2026-02-20 09:33:00 +0100

    Library app

---
+++ library.py
@@ -0,0 +1,18 @@
+# Make a library app, to manage books for our students in the library. 
+# We want to be able to add books, remove books, and list all the books in the library.
+
+#structure:
+
+#imports:
+
+# global variables and constants:
+
+# functions:
+
+# main loop:
+def main():
+    print("Welcome to the library app!")
+    # main loop that runs all the time, until the user exits.
+
+#execution:
+main()
